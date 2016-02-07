3f71425f18a0f6713860bb1127a7038ec1a6a2d0
Benjamin Adamson <adamson.benjamin@gmail.com>
2016-02-07 14:05:44 -0800

    Move build artifacts to build directory.

--- build.sh
+++ build.sh
@@ -4,4 +4,6 @@ source ~/Downloads/dotfiles/zsh/alias
 export HANA="/home/benjamin/Downloads/hana/include/"
 export CPPFMT="/home/benjamin/Downloads/cppformat/"
 export CPPFMT_LD="/home/benjamin/Downloads/cppformat/build/cppformat/"
-clang++ --std=c++14 -stdlib=libstdc++ $@ -static -lstdc++ -I$CPPFMT -L$CPPFMT_LD -lcppformat
+clang++ --std=c++14 -stdlib=libstdc++ $@ -static -o build/a.out -lstdc++ \
+  -I$CPPFMT \
+  -L$CPPFMT_LD -lcppformat
